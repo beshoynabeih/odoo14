b32047c05abda4627df3a69ac92919eee890f127
Beshoy Nabeih <beshoybosha231@gmail.com>
2021-04-17 17:37:12 +0200

    add index.html file

--- menu_external_id/__manifest__.py
+++ menu_external_id/__manifest__.py
@@ -10,7 +10,7 @@
     """,
 
     'author': "Beshoy Nabeih",
-    'website': "http://www.yourcompany.com",
+    'website': "https://www.linkedin.com/in/beshoynabeih/",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
